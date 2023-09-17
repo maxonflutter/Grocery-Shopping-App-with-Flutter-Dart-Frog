89ec355f6b1107c45b1c9a7331a14eef145c7a3e
Max on Flutter <42350837+maxonflutter@users.noreply.github.com>
2023-09-17 18:05:25 +0400

    First commit

---
+++ android/app/src/main/kotlin/com/example/grocery_shopping_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.grocery_shopping_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ api/packages/models/lib/models.dart
@@ -0,0 +1,7 @@
+library models;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
